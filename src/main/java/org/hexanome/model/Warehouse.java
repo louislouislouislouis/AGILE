364ed675a82491b9f6b9c0ddbcdc2cd8f226fb83
Raphael Vignon <65215675+Rafouf69@users.noreply.github.com>
2021-11-21 22:17:31 +0100

    transformation en localTime

--- src/main/java/org/hexanome/model/Warehouse.java
+++ src/main/java/org/hexanome/model/Warehouse.java
@@ -1,14 +1,12 @@
 package org.hexanome.model;
 
-public class Warehouse extends Point{
-    private int departureHour;
-    private int departureMinute;
-    private int departureSecond;
+import java.time.LocalTime;
 
-    public Warehouse(int departureHour, int departureMinute, int departureSecond, Intersection departureAddress) {
+public class Warehouse extends Point{
+    private LocalTime departureTime;
+
+    public Warehouse(LocalTime departureTime, Intersection departureAddress) {
         super(departureAddress);
-        this.departureHour = departureHour;
-        this.departureMinute = departureMinute;
-        this.departureSecond = departureSecond;
+        this.departureTime = departureTime;
     }
 }
