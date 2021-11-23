163aad43476afd8235b8b4b232c97a3b8f45410e
Raphael Vignon <65215675+Rafouf69@users.noreply.github.com>
2021-11-23 10:01:24 +0100

    fileChooser

--- src/main/java/org/hexanome/controller/MainsScreenController.java
+++ src/main/java/org/hexanome/controller/MainsScreenController.java
@@ -1,13 +1,23 @@
 package org.hexanome.controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.hexanome.vue.App;
 
 public class MainsScreenController {
     public void selectionMap(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        Stage thisStage = (Stage) node.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(thisStage);
+
+        System.out.println(selectedFile);
     }
 
     public void addRequest(ActionEvent actionEvent) {
