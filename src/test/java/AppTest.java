74c53e6e7c89d9ce38b399bb96f89a96c72bbba8
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2021-11-23 08:26:45 +0100

    Test quantitatifs basiques du parser XML

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,14 +1,31 @@
+import org.hexanome.data.MapDeserializer;
+import org.hexanome.model.MapIF;
 import org.junit.Test;
+
+import java.io.File;
+
 import static org.junit.Assert.assertEquals;
 
 public class AppTest {
-    @Test
-    public void testAdd() {
-        String str = "Junit is working fine";
-        assertEquals("CACA", str);
-    }
-
     // TODO : Test du parser XML
-    //
+    @Test
+    public void testXML() {
+        MapDeserializer md = new MapDeserializer();
+        MapIF map = new MapIF();
+
+        try {
+            File fileXml = new File("src/main/resources/org/hexanome/model/testMap.xml");
+            md.load(map, fileXml);
+
+            // Test quantitatif
+            assertEquals(map.getSegments().size(), 2);
+            assertEquals(map.getIntersections().size(), 4);
+
+            //Test qualitatif
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 
 }
