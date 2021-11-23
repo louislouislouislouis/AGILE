0e182effc29a470eb29e00267a3d95b249d31b83
Angélica Gutiérrez Moreno <angelica.gumo20@gmail.com>
2021-11-23 09:38:39 +0100

    Controller: creation des methodes button

--- src/main/java/org/hexanome/controller/MainsScreenController.java
+++ src/main/java/org/hexanome/controller/MainsScreenController.java
@@ -2,10 +2,16 @@ package org.hexanome.controller;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import org.hexanome.vue.App;
 
 public class MainsScreenController {
+    public void selectionMap(ActionEvent actionEvent) {
+    }
+
+    public void addRequest(ActionEvent actionEvent) {
+    }
     /**@FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
