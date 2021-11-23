60cd9fd562928a30d08c447116d98d4e3f7db080
Kinepo <alexandre.senouci@insa-lyon.fr>
2021-11-23 09:25:10 +0100

    ihm qui s'ouvre

---
+++ src/main/java/org/hexanome/controller/MainsScreenController.java
@@ -0,0 +1,13 @@
+package org.hexanome.controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import org.hexanome.vue.App;
+
+public class MainsScreenController {
+    /**@FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    } **/
+}
