e61c4d62beb15539fa5dd88acc134c6a8a9c2092
Angélica Gutiérrez Moreno <angelica.gumo20@gmail.com>
2021-11-23 11:15:25 +0100

    MainsScreenController-creation du methode FileChooser

--- src/main/java/org/hexanome/controller/MainsScreenController.java
+++ src/main/java/org/hexanome/controller/MainsScreenController.java
@@ -1,26 +1,63 @@
 package org.hexanome.controller;
 
 import java.io.File;
-import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.hexanome.vue.App;
 
 public class MainsScreenController {
+
+    /*---------------------------VARIABLES------------------------------------------------------------*/
+
+    //Declaration of the buttons interactives in the mainsScreen.fxml
+    @FXML private Button btnLoadMap;
+    @FXML private Button btnAddRequest;
+    @FXML private Button btnValidateRoute;
+
+    /*-------------------------GETTERS AND SETTERS-----------------------------------------------------*/
+    public Button getBtnLoadMap() {return btnLoadMap;}
+    public void setBtnLoadMap(Button btnLoadMap) {this.btnLoadMap = btnLoadMap;}
+    public Button getBtnAddRequest() {return btnAddRequest;}
+    public void setBtnAddRequest(Button btnAddRequest) {this.btnAddRequest = btnAddRequest;}
+    public Button getBtnValidateRoute() {return btnValidateRoute;}
+    public void setBtnValidateRoute(Button btnValidateRoute) {this.btnValidateRoute = btnValidateRoute;}
+
+    /*--------------------------------Methods----------------------------------------------------------*/
     public void selectionMap(ActionEvent actionEvent) {
+        //method that uploads an XML file (carte)
+/*        Node node = (Node) actionEvent.getSource();
+        Stage thisStage = (Stage) node.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(thisStage);*/
+
+        File selectedFile = fileChooser(actionEvent);
+        System.out.println(selectedFile);
+        while(selectedFile.exists()){
+            btnAddRequest.setDisable(false);
+            btnValidateRoute.setDisable(false);
+        }
+    }
+
+    public void addRequest(ActionEvent actionEvent) {
+        //method that uploads an XML file with the command
+    }
+
+    public void calculateRoute(ActionEvent actionEvent) {
+        //method that calculates the most optimal path of the tour
+    }
+
+    public File fileChooser(ActionEvent actionEvent){
+        //method that opens the File Explorer to allow the user to get their XML files
         Node node = (Node) actionEvent.getSource();
         Stage thisStage = (Stage) node.getScene().getWindow();
         FileChooser fileChooser = new FileChooser();
         File selectedFile = fileChooser.showOpenDialog(thisStage);
 
-        System.out.println(selectedFile);
-    }
-
-    public void addRequest(ActionEvent actionEvent) {
+        return selectedFile;
     }
     /**@FXML
     private void switchToSecondary() throws IOException {
