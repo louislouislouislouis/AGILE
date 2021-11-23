6c94a8121d11b76cbd92ab0945d33b1e6aad61d0
Raphael Vignon <65215675+Rafouf69@users.noreply.github.com>
2021-11-23 10:08:52 +0100

    changement du type de la clé de la hashmap string->UUID

--- src/main/java/org/hexanome/model/MapIF.java
+++ src/main/java/org/hexanome/model/MapIF.java
@@ -2,12 +2,13 @@ package org.hexanome.model;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class MapIF {
     private HashMap<Long, Intersection> intersections;
-    private HashMap<String, Segment> segments;
+    private HashMap<UUID, Segment> segments;
 
-    public MapIF(HashMap<Long, Intersection> intersections, HashMap<String, Segment> segments) {
+    public MapIF(HashMap<Long, Intersection> intersections, HashMap<UUID, Segment> segments) {
         this.intersections = intersections;
         this.segments = segments;
     }
@@ -17,8 +18,8 @@ public class MapIF {
         this.segments = new HashMap<>();
     }
 
-    public void addSegment(Segment segment){
-        this.segments.put(segment.getName(), segment);
+    public void addSegment(UUID id, Segment segment){
+        this.segments.put(id, segment);
     }
 
     public void addIntersection(Intersection intersection){
@@ -29,7 +30,7 @@ public class MapIF {
         return intersections;
     }
 
-    public HashMap<String, Segment> getSegments() {
+    public HashMap<UUID, Segment> getSegments() {
         return segments;
     }
 
@@ -41,8 +42,8 @@ public class MapIF {
             toReturn += pair.getValue().toString() + "\n";
         }
         toReturn += ",\nsegments=\n";
-        for (Map.Entry<String, Segment> pair : segments.entrySet()) {
-            toReturn += pair.getValue().toString() + "\n";
+        for (Map.Entry<UUID, Segment> pair : segments.entrySet()) {
+            toReturn += "id: "+pair.getKey()+" "+pair.getValue().toString() + "\n";
         }
         toReturn += '}';
         return toReturn;
