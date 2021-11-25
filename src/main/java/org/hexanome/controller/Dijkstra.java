f7ffa84821a748859d27ee7c37075c2c2a9f574d
huy.tran <huy.tran@spiraledge.com>
2021-11-26 00:34:52 +0100

    Dijkstra and TSP

--- src/main/java/org/hexanome/controller/Dijkstra.java
+++ src/main/java/org/hexanome/controller/Dijkstra.java
@@ -26,28 +26,31 @@ public class Dijkstra {
     /**
      * calculates the shortest paths from a given originIntersection to all given destinationIntersections
      * @param intersections amount of all intersections in the map
-     * @param segments amount of all segments in the map
+     * @param adj Adjacency matrix
      * @param origin origin intersection
      * @param destinations set of destinations intersections
      * @return map with key: idDestinationIntersection,
      *         value: list of intersections from originIntersection to destinationIntersection
      */
-    public Map<Long,List<Long>> dijkstra(Map<Long,Intersection> intersections, Map<UUID,Segment> segments,
-                         Intersection origin, Set<Intersection> destinations) {
+    public Map<Long,List<Long>> dijkstra(
+            Map<Long,Intersection> intersections,
+            Map<Intersection,Map<Intersection,Segment>> adj,
+            Intersection origin,
+            Set<Intersection> destinations) {
 
-        this.adj = getAdj(intersections, segments); // overview over intersections and their neighbours
+        this.adj = adj;
 
         for (Intersection i : intersections.values()) {
             List<Long> initialPath = new ArrayList<>();
+            // no path to go to intersections
             if (i.getIdIntersection() == origin.getIdIntersection()) {
                 this.dist.put(i.getIdIntersection(), (double) 0); // startIntersection has currentWeight 0
                 initialPath.add(origin.getIdIntersection());
-                this.path.put(i.getIdIntersection(),initialPath); // add startIntersection
             }
             else {
                 this.dist.put(i.getIdIntersection(),(double) Integer.MAX_VALUE); // other intersections (not start)
-                this.path.put(i.getIdIntersection(), initialPath); // no path to go to intersections
             }
+            this.path.put(i.getIdIntersection(),initialPath); // add startIntersection
         }
 
         this.pq.add(new Node(origin.getIdIntersection(), (double) 0)); // add startIntersection to pq
@@ -66,26 +69,6 @@ public class Dijkstra {
             exploreNeighbours(currentNode);
         }
         return getResult(destinations);
-    }
-
-    /**
-     *
-     * @param intersections amount of all intersections in the map
-     * @param segments amount of all segments in the map
-     * @return map with key: startIntersection,
-     *         value: map with key: neighbourIntersection,
-     *                value: segment between startIntersection and neighbourIntersection
-     */
-    public Map<Intersection,Map<Intersection,Segment>> getAdj(Map<Long,Intersection> intersections, Map<UUID,Segment> segments) {
-        Map<Intersection,Map<Intersection,Segment>> adj = new HashMap<>();
-        for (Intersection i: intersections.values()) {
-            Map<Intersection,Segment> emptyMap = new HashMap<>();
-            adj.put(i,emptyMap);
-        }
-        for (Segment s: segments.values()) {
-            adj.get(s.getOriginIntersection()).put(s.getDestinationIntersection(),s);
-        }
-        return adj;
     }
 
     /**
@@ -134,7 +117,20 @@ public class Dijkstra {
         return result;
     }
 
+    public Map<Long, Double> getResultDistForTSP(Set<Intersection> destinations) {
+        Map<Long, Double> result = new HashMap<>();
+
+        for (Intersection i : destinations) {
+            result.put(i.getIdIntersection(), dist.get(i.getIdIntersection()));
+        }
+        return result;
+    }
+
     public Map<Long, Double> getDist() {
         return dist;
+    }
+
+    public Map<Long, List<Long>> getPath() {
+        return path;
     }
 }
--- src/main/java/org/hexanome/data/MapDeserializer.java
+++ src/main/java/org/hexanome/data/MapDeserializer.java
@@ -1,5 +1,7 @@
 package org.hexanome.data;
 
+import org.hexanome.controller.tsp.CompleteGraph;
+import org.hexanome.controller.tsp.Graph;
 import org.hexanome.model.*;
 import org.hexanome.controller.Dijkstra;
 import org.w3c.dom.Document;
@@ -13,12 +15,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 import java.lang.*;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 public class MapDeserializer {
     /**
@@ -93,6 +91,26 @@ public class MapDeserializer {
         return new Segment(originIntersection,destinationIntersection,name, length);
     }
 
+    /**
+     *
+     * @param intersections amount of all intersections in the map
+     * @param segments amount of all segments in the map
+     * @return map with key: startIntersection,
+     *         value: map with key: neighbourIntersection,
+     *         value: segment between startIntersection and neighbourIntersection
+     */
+    private Map<Intersection, Map<Intersection,Segment>> getAdj(Map<Long,Intersection> intersections, Map<UUID,Segment> segments) {
+        Map<Intersection,Map<Intersection,Segment>> adj = new HashMap<>();
+        for (Intersection i: intersections.values()) {
+            Map<Intersection,Segment> emptyMap = new HashMap<>();
+            adj.put(i,emptyMap);
+        }
+        for (Segment s: segments.values()) {
+            adj.get(s.getOriginIntersection()).put(s.getDestinationIntersection(),s);
+        }
+        return adj;
+    }
+
     public static void main(String args[]) {
         MapDeserializer mydom = new MapDeserializer();
         RequestDeserializer mydomrequest = new RequestDeserializer();
@@ -101,16 +119,12 @@ public class MapDeserializer {
 
         try {
             File fileXml = new File("src/main/resources/org/hexanome/model/testMap.xml");
-
             mydom.load(map, fileXml);
 
-            System.out.println(map);
-
             File requestFile = new File("src/main/resources/org/hexanome/model/testRequest.xml");
-
             mydomrequest.load(planning, requestFile, map);
 
-            System.out.println(planning);
+            Map<Intersection, Map<Intersection,Segment>> adj = mydom.getAdj(map.getIntersections(), map.getSegments());
 
             Set<Intersection> destinations = new HashSet<>();
             destinations.add(planning.getWarehouse().getAddress());
@@ -119,12 +133,33 @@ public class MapDeserializer {
                 destinations.add(r.getDeliveryPoint().getAddress());
             }
 
+            int nbVerticesDijkstra = map.getIntersections().size();
+            int nbVerticesTSP = destinations.size();
+
+            Double [][] costTSP = new Double[nbVerticesTSP][nbVerticesTSP];
+            int iOrigin = 0;
             for (Intersection origin : destinations) {
-                System.out.println("Calculating shortest paths for Origin: " + origin);
-                Dijkstra dijkstra = new Dijkstra(map.getIntersections().size());
-                //System.out.println(dijkstra.dijkstra(map.getIntersections(),map.getSegments(),origin,destinations));
+                System.out.println("Calculating shortest paths for Origin: " + origin.getIdIntersection());
+                Dijkstra dijkstra = new Dijkstra(nbVerticesDijkstra);
+                dijkstra.dijkstra(map.getIntersections(), adj, origin, destinations);
+                System.out.println(dijkstra.getPath());
                 System.out.println(dijkstra.getDist());
+
+                Map <Long, Double> distTSP = dijkstra.getResultDistForTSP(destinations);
+                int j = 0;
+                for (Double c : distTSP.values()) {
+                    costTSP[iOrigin][j++] = c;
+                }
+                iOrigin++;
             }
+
+            for (int i = 0; i < nbVerticesTSP; i++) {
+                for (int j = 0; j < nbVerticesTSP; j++) {
+                    System.out.print(costTSP[i][j] + " ");
+                }
+                System.out.println();
+            }
+
 
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
@@ -136,4 +171,6 @@ public class MapDeserializer {
             exceptionXML.printStackTrace();
         }
     }
+
+
 }
