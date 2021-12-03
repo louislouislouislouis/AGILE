18b662bb2de3b0d472e51bf37570c9379c24bf50
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2021-12-03 09:30:44 +0100

    Lecture du fichier solution

--- src/test/java/TSPTest.java
+++ src/test/java/TSPTest.java
@@ -1,27 +1,27 @@
-import org.hexanome.model.Intersection;
-import org.hexanome.model.MapIF;
-import org.hexanome.model.PlanningRequest;
-import org.hexanome.model.Segment;
+import javafx.scene.paint.Color;
+import org.hexanome.model.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.UUID;
 
+import static java.lang.Long.parseLong;
 import static org.junit.Assert.*;
 
 public class TSPTest {
     protected static HashMap<Long, Intersection> intersections;
     protected static HashMap<UUID, Segment> segments;
     protected static MapIF map;
-    protected static PlanningRequest planning
+    protected static PlanningRequest planning;
+    protected static Long[] solution;
 
     @BeforeClass
     public static void setUp() {
         String path = "src/test/tsplib/a280.tsp";
-        File a280 = new File(path);
         try {
             FileReader fr = new FileReader(path);
             BufferedReader br = new BufferedReader(fr);
@@ -39,18 +39,42 @@ public class TSPTest {
 
             // Relier toutes les intersections
             segments = new HashMap<>();
-            intersections.forEach((id1, intersection1) -> {
-                intersections.forEach((id2, intersection2) -> {
-                    if (!Objects.equals(id1, id2)) {
-                        segments.put(UUID.randomUUID(), new Segment(intersection1, intersection2, "Rue de Clignancourt", 42.0));
-                    }
-                });
-            });
+            intersections.forEach((id1, intersection1) -> intersections.forEach((id2, intersection2) -> {
+                if (!Objects.equals(id1, id2)) {
+                    segments.put(UUID.randomUUID(), new Segment(intersection1, intersection2, "Rue de Clignancourt", 42.0));
+                }
+            }));
 
             map = new MapIF(intersections, segments);
 
             // Génération du planning
+            planning = new PlanningRequest();
+            Intersection warehouse = intersections.get(1L);
+            planning.setWarehouse(new Warehouse(LocalTime.MIDNIGHT, warehouse, Color.GREEN));
+            intersections.forEach((id, intersection) -> {
+                PickupPoint pp = new PickupPoint(warehouse, 0, Color.RED);
+                DeliveryPoint dp = new DeliveryPoint(intersection, 0, Color.RED);
+                planning.addRequest(new Request(pp, dp));
+            });
 
+            // Lecture du fichier solution
+            String pathSol = path.replace(".tsp", ".opt.tour");
+            FileReader frSol = new FileReader(pathSol);
+            BufferedReader brSol = new BufferedReader(frSol);
+            for (int i = 0; i < 4; ++i) {
+                brSol.readLine();
+            }
+            solution = new Long[280];
+            for (int i = 0; i < 280; ++i) {
+                solution[i] = parseLong(brSol.readLine());
+            }
+            assertEquals((long) solution[0], 1);
+            assertEquals((long) solution[17], 245);
+            assertEquals((long) solution[279], 280);
+            assertEquals((long) solution[195], 36);
+            assertEquals((long) solution[141], 115);
+            assertEquals((long) solution[100], 162);
+            assertEquals((long) solution[11], 235);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -60,7 +84,7 @@ public class TSPTest {
 
     @Test
     public void testTSP() {
-        System.out.println("[TSPTest] fumier");
+        System.out.println("[TSPTest] RAS");
     }
 
 }
