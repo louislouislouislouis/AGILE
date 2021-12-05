ccfe96293efde94440b3f725f1379aa04a2c843e
Raphael Vignon <65215675+Rafouf69@users.noreply.github.com>
2021-12-05 22:17:46 +0100

    add request bien avancé la requete est ajouté mais il faut mettre à jour l'affichage et appeler l'algo

--- src/main/java/org/hexanome/controller/AddRequestState1.java
+++ src/main/java/org/hexanome/controller/AddRequestState1.java
@@ -8,6 +8,30 @@ public class AddRequestState1 implements State {
 
     @Override
     public void leftClick(MainsScreenController controller, Intersection i) {
+        System.out.println(i);
 
+        if (i != null) {
+            // we change the selected intersection in the next state
+            controller.addRequestState2.setSelectedIntersection(i);
+            
+            // we change the state of the controller
+            controller.setCurrentState(controller.addRequestState2);
+        } else {
+
+        }
+    }
+
+    @Override
+    public void rightClick(MainsScreenController controller) {
+        // we change the state of the controller
+        controller.setCurrentState(controller.tourState);
+    }
+
+    @Override
+    public void enableButton(MainsScreenController controller) {
+        controller.getBtnLoadRequest().setDisable(true);
+        controller.getBtnLoadMap().setDisable(true);
+        controller.getBtnValidateRoute().setDisable(true);
+        controller.getBtnAddRequest().setDisable(true);
     }
 }
--- src/main/java/org/hexanome/controller/AddRequestState2.java
+++ src/main/java/org/hexanome/controller/AddRequestState2.java
@@ -1,4 +1,51 @@
 package org.hexanome.controller;
 
-public class AddRequestState2 {
+import javafx.scene.paint.Color;
+import org.hexanome.model.*;
+
+public class AddRequestState2 implements State {
+
+    Intersection selectedIntersection;
+
+    public void setSelectedIntersection(Intersection i) {
+        selectedIntersection = i;
+    }
+
+    @Override
+    public void leftClick(MainsScreenController controller, Intersection i) {
+        System.out.println(i);
+
+        if (i != null) {
+            Color color = ColorEnum.values()[controller.getPlanning().getRequests().size()].color;
+
+            // we lack 2 states for each duration time
+
+            PickupPoint pickupPoint = new PickupPoint(selectedIntersection, 10, color);
+            DeliveryPoint deliveryPoint = new DeliveryPoint(i, 10, color);
+
+            Request request = new Request(pickupPoint, deliveryPoint);
+
+            controller.getPlanning().addRequest(request);
+            
+            // we change the state of the controller
+            controller.setCurrentState(controller.tourState);
+        } else {
+
+        }
+    }
+
+    @Override
+    public void rightClick(MainsScreenController controller) {
+        // we change the state of the controller
+        controller.setCurrentState(controller.tourState);
+    }
+
+    @Override
+    public void enableButton(MainsScreenController controller) {
+        controller.getBtnLoadRequest().setDisable(true);
+        controller.getBtnLoadMap().setDisable(true);
+        controller.getBtnValidateRoute().setDisable(true);
+        controller.getBtnAddRequest().setDisable(true);
+    }
+
 }
