a04f24132dd8ba681fe2b61999b731d755c71b74
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2021-12-13 21:45:51 +0100

    Ajout de RunTestSuite (pour exécuter tous les tests d'un coup)

---
+++ src/test/java/RunTestSuite.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({IntersectionTest.class, TSPTest.class, TSPTrivialTest.class, XMLTest.class})
+public class RunTestSuite {
+}
