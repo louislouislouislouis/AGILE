b11c654eeff426eee55e4fcd0d3c7230ef5896f2
Angélica Gutiérrez Moreno <angelica.gumo20@gmail.com>
2021-12-13 19:45:09 +0100

    Edit function done
    Missing updating point in the table

--- src/main/java/org/hexanome/controller/ModifyRequestState.java
+++ src/main/java/org/hexanome/controller/ModifyRequestState.java
@@ -1,35 +1,23 @@
 package org.hexanome.controller;
 
-import org.hexanome.model.Intersection;
-import org.hexanome.model.Point;
-import org.hexanome.model.Tour;
+import org.hexanome.model.*;
+import org.hexanome.vue.ExceptionBox;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class ModifyRequestState implements State {
-    /*
-            Missing tableview is not updating
-            Missing refresh points in map
-         */
+    Request oldRequest;
+    Request newRequest;
+    DeliveryPoint oldDeliveryPoint;
+    PickupPoint oldPickupPoint;
+    DeliveryPoint newDeliveryPoint;
+    PickupPoint newPickupPoint;
+    Point pointToUpdate;
     @Override
     public void leftClick(MainsScreenController controller, Intersection i) throws Exception {
         if (i != null) {
-            System.out.println(i);
-            Point pointToUpdate = controller.getTableView().getSelectionModel().getSelectedItem();
-            pointToUpdate.setAddress(i);
-            controller.getMapView().removeLayer(controller.getIntersectionLayer());
-            controller.setCurrentState(controller.tourState);
-            controller.updateTableView();
-            controller.updateRequestLayer();
-            controller.updateTourLayer();
-            controller.tour = new Tour(new ArrayList<>(), null, controller, controller.planning.getWarehouse().getDepartureTime(), controller.map.getMatAdj());
-            // we must reset the command list
-            controller.listOfCommands.reset();
-            // we compute the tour
-            controller.currentState.computeTour(controller, controller.map, controller.planning, controller.tour);
-            controller.updateMapView();
-
-
+            clickEdit(controller, i, controller.listOfCommands);
         }
     }
 
@@ -45,5 +33,34 @@ public class ModifyRequestState implements State {
         controller.getBtnStopCalcul().setDisable(false);
         controller.getBtnDeleteTableRow().setDisable(false);
         controller.getBtnEditTableRow().setDisable(false);
+    }
+
+    public void clickEdit(MainsScreenController controller, Intersection i, ListOfCommands listOfCommands){
+        System.out.println(i);
+        pointToUpdate = controller.getTableView().getSelectionModel().getSelectedItem();
+        System.out.println("Point to uptdate: " + pointToUpdate);
+        LinkedList<Request> requestList = controller.getPlanning().getRequests();
+        oldRequest = requestList.element();
+        if(pointToUpdate.getType().equals("pickup")){
+            oldDeliveryPoint = oldRequest.getDeliveryPoint();
+            oldPickupPoint = oldRequest.getPickupPoint();
+            newPickupPoint = new PickupPoint(pointToUpdate.getAddress(), oldPickupPoint.getDuration(), oldPickupPoint.getColor());
+            newPickupPoint.setAddress(i);
+            newRequest = new Request(newPickupPoint,oldDeliveryPoint);
+        }else if(pointToUpdate.getType().equals("delivery")){
+            oldDeliveryPoint = oldRequest.getDeliveryPoint();
+            oldPickupPoint = oldRequest.getPickupPoint();
+            newDeliveryPoint = new DeliveryPoint(pointToUpdate.getAddress(), oldDeliveryPoint.getDuration(), oldDeliveryPoint.getColor());
+            newDeliveryPoint.setAddress(i);
+            newRequest = new Request(oldPickupPoint,newDeliveryPoint);
+        }else if(pointToUpdate.getType().equals("warehouse")){
+            new ExceptionBox("Cannot change warehouse", "Behavioral problem").display();
+            newRequest = oldRequest;
+        }
+        listOfCommands.add(new DeleteRequestCommand(controller, oldRequest));
+        listOfCommands.add(new AddRequestCommand(controller, newRequest));
+        controller.getMapView().removeLayer(controller.getIntersectionLayer());
+        // we change the state of the controller
+        controller.setCurrentState(controller.tourState);
     }
 }
